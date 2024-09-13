9db9ac3977b770a61b3cb04bf8c945a9deccaa35
Kevin Cao <kcao36@outlook.comgit config --global user.email kcao36@outlook.comclear>
2024-09-12 22:28:21 -0600

    Add UI to game

--- script.js
+++ script.js
@@ -26,80 +26,108 @@ function getHumanChoice(){
 
 
 function playGame(){
+
+    const body = document.querySelector('body')
+
+    const buttonDiv = document.createElement('div')
+
+    const rock = document.createElement('button')
+    rock.textContent = "Rock"
+    rock.addEventListener("click", function (){
+        playRound('rock',getComputerChoice())
+})
+
+
+    const paper = document.createElement('button')
+    paper.textContent = "Paper"
+    paper.addEventListener("click", function (){
+        playRound('paper',getComputerChoice())
+    })
+
+    const scissors = document.createElement('button')
+    scissors.textContent = "Scissors"
+    scissors.addEventListener("click", function(){
+        playRound('scissors',getComputerChoice())
+    })
+
+
+    buttonDiv.appendChild(rock)
+    buttonDiv.appendChild(paper)
+    buttonDiv.appendChild(scissors)
+
+    body.appendChild(buttonDiv)
+
     let humanScore = 0
     let computerScore = 0
 
     function playRound(humanChoice,computerChoice){
+
+        if (humanScore == 5 || computerScore == 5){
+            const finalResult = document.createElement('div')
+            finalResult.textContent = humanScore == 5 ? 'You won the game!' : 'You lost the game!'
+            body.appendChild(finalResult)
+            body.empty()
+
+        }
+
+        const compChoice = document.createElement('div')
+        const result = document.createElement('div')
+
         // Doing all case where human wins first
         if (humanChoice == 'rock' && computerChoice == 'scissors'){
             humanScore = ++humanScore
-            console.log('You win! Rock beats scissors!')        
+            compChoice.textContent = 'scissors'
+            result.textContent = 'You win! Rock beats scissors!'
+            body.appendChild(compChoice)
+            body.appendChild(result)
         }
         else if (humanChoice == 'paper' && computerChoice == 'rock'){
             humanScore = ++humanScore
-            console.log('You win! Paper beats scissors!')
+            compChoice.textContent = 'rock'
+            result.textContent = 'You win! Paper beats rock!'
+            body.appendChild(compChoice)
+            body.appendChild(result)
         }
         else if (humanChoice == 'scissors' && computerChoice == 'paper'){
             humanScore = ++humanScore
-            console.log('You win! Scissors beats paper!')
+            compChoice.textContent = 'paper'
+            result.textContent = 'You win! Scissors beats paper!'
+            body.appendChild(compChoice)
+            body.appendChild(result)
         }
         // Now doing computer wins
         else if (computerChoice == 'rock' && humanChoice == 'scissors'){
             computerScore = ++computerScore
-            console.log('You lose! Rock beats scissors!')
+            compChoice.textContent = 'rock'
+            result.textContent = 'You lose! Rock beats scissors!'
+            body.appendChild(compChoice)
+            body.appendChild(result)
         }
         else if (computerChoice == 'paper' && humanChoice == 'rock'){
             computerScore = ++computerScore
-            console.log('You lose! Paper beats rock!')
+            compChoice.textContent = 'paper'
+            result.textContent = 'You lose! Paper beats rock!'
+            body.appendChild(compChoice)
+            body.appendChild(result)
         }
         else if (computerChoice == 'scissors' && humanChoice == 'paper'){
             computerScore = ++computerScore
-            console.log('You lose! Scissors beats paper!')
+            compChoice.textContent = 'scissors'
+            result.textContent = 'You lose! Scissors beats paper!'
+            body.appendChild(compChoice)
+            body.appendChild(result)
         }
         // Tie
         else if (humanChoice==computerChoice){
-            humanScore = ++humanScore
-            computerScore = ++computerScore
-            console.log('Tie!')
+            result.textContent = 'Tie!'
 
         }
     }
-
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
-
-    playRound(humanChoice,computerChoice)
-
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-
-    playRound(humanChoice,computerChoice)
-
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-
-    playRound(humanChoice,computerChoice)
-
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-
-    playRound(humanChoice,computerChoice)
-
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerChoice()
-
-    playRound(humanChoice,computerChoice)
-
-    console.log('Your score: ' + humanScore)
-    console.log('CPU score: ' + computerScore)
-
-    if (humanScore > computerScore){
-        console.log('You win!')
-    }
-    else{
-        console.log('You lose!')
-    }
 }
 
+
+
 playGame()
+
+
 
